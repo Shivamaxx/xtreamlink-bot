b848804666c85f2f3e7b0329a1ec23d8d30ee3ba
Shivamaxx <166101974+Shivamaxx@users.noreply.github.com>
2026-05-17 15:06:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,6 +74,8 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
             'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
             'quiet': True,
             'no_warnings': True,
+            'username': 'oauth2',
+            'password': '',
             'extractor_args': {
                 'youtube': {
                     'player_client': ['android'],
