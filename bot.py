b917d5feaeb03cbb31355439c12513fa5f73ac35
Shivamaxx <166101974+Shivamaxx@users.noreply.github.com>
2026-05-17 11:18:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
             }
         else:
             ydl_opts = {
-                'format': f'best[height<={quality}]',
+                'format': f'bestvideo[height<={quality}]+bestaudio/best/best',
                 'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
                 'merge_output_format': 'mp4',
                 'quiet': True,
